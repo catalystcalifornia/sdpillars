f77a4123512e012e28aef6f67c4429286af29168
Elycia Mulholland Graves <egraves@advanceproj.org>
2023-08-09 16:53:31 -0700

    saving qa of descriptive

--- descriptive_analysis/qa.R
+++ descriptive_analysis/qa.R
@@ -2,4 +2,9 @@ qa_beats<-beats%>%group_by(beat,div)%>%summarise(count=n())
 unique_beats<-length(unique(qa_beats$beat))
 beats_div_qa<-qa_beats%>%
   left_join(div, by=c("div"="div_num"))
+qa<-beats_div_qa%>%
+  left_join(beats_div,by="beat")
+%>%
+  mutate(ifelse(div_name.x!=div_name.y))
+
 
