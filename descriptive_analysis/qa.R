17a175e3da226def4b92187cb7f93df9a1ec1b6a
Elycia Mulholland Graves <egraves@advanceproj.org>
2023-08-10 11:28:52 -0700

    Edits to stop reason by police assignment + qa code

--- descriptive_analysis/qa.R
+++ descriptive_analysis/qa.R
@@ -4,7 +4,50 @@ beats_div_qa<-qa_beats%>%
   left_join(div, by=c("div"="div_num"))
 qa<-beats_div_qa%>%
   left_join(beats_div,by="beat")
-%>%
-  mutate(ifelse(div_name.x!=div_name.y))
 
 
+qa<-beats_stop%>%
+  filter(is.na(div_name))
+
+
+qa<-beats_stop%>%
+  filter(Year=='2022')%>%
+  group_by(div_name,stop_in_response_to_cfs)%>%
+  summarise(Count=n(),
+            'Average Stop Time (Mins)' = mean(stopduration, na.rm = FALSE),
+            'Median Stop Time (Mins)'=   median(stopduration,na.rm = FALSE),
+            'Min Stop Time (Mins)'=min(stopduration,na.rm = FALSE),
+            'Max Stop Time (Mins)'=max(stopduration,na.rm = FALSE),
+            'Total Time (Mins)'= sum(stopduration)
+  )
+
+qa<-total<-stops%>%
+  filter(stop_in_response_to_cfs==0 & Year=='2022')%>%nrow()
+
+qa<-stops%>%
+  filter(stop_in_response_to_cfs==0 & Year=='2022')%>%
+  group_by(assignment)%>%
+  summarise(Count=n())
+qa_total<-beats_stop%>%
+  filter(Year=='2022')%>%
+  group_by(div_name)%>%
+  summarise(Total=n())
+  
+qa<-beats_stop%>%
+  filter(Year=='2022')%>%
+  group_by(div_name,stop_in_response_to_cfs)%>%
+  summarise(Count=n())%>%
+  left_join(qa_total)%>%
+  mutate(Percent=Count/Total*100)
+
+qa<-reason_p %>%
+  group_by(stop_id, person_id) %>% 
+  summarise(count=n())%>%
+  nrow()
+
+reason_p%>%filter(is.na(reason_for_stop))%>%nrow()
+
+
+reason_p_dedup<-reason_p%>%
+  group_by(stop_id,person_id,reason_for_stop,assignment)%>%
+  summarise(duplicates=n())
