af4bc8581c74b48d23e7e6f6fb18e198ddaf97ee
Hillary Khan <HKhan@catalystcalifornia.org>
2023-10-26 17:11:31 -0700

    added item chart function and testing

--- report_embeds/chart_functions.R
+++ report_embeds/chart_functions.R
@@ -252,7 +252,7 @@ fx_bubblepopchart(
 dbDisconnect(pillars_conn)
 
 #### Stacked Bar Chart ####
-fx_stack <- function(
+fx_stackedbarchart <- function(
     df,
     x, # independent variable
     y, # dependent variable
@@ -312,7 +312,7 @@ stoprates_age_race_ficard_person$race<-factor(stoprates_age_race_ficard_person$r
 
 tooltip_text <- "Across all field interviews of folks <b> ages {point.age_bracket:.1f} </b>, SDPD stopped <b>{point.race:.1f} people {point.rate:.1f}% of the time.</b>"
 
-fx_stack(  
+fx_stackedbarchart(  
   df = stoprates_age_race_ficard_person,
   x = 'age_bracket',
   y = 'rate',
@@ -327,194 +327,71 @@ fx_stack(
 #### Item Chart ####
 fx_itemchart <- function(
     df, # name of dataframe
-    x, # x or independent variables - will appear on y-axis
-    y, # y or dependent variable - will appear on x-axis
-    z, # variable storing count - will determine bubble size
+    x, # category - will appear on y-axis
+    y, # rate - will appear on x-axis
+    group_colors, # vector of color values
+    row_nums, # integer
     top_finding="",
     subtitle= "",
     tooltip_text="",
-    caption = "",
-    yaxis_label = "''",
-    export_data_label="") {
-  
-  yaxis_label_JS <- paste0("function() {
-        	return this.value +", yaxis_label, "}")
+    legend_text="", # states what 1 dot represents
+    caption = "") {
+
   
   # add line breaks to tooltip_text
   tooltip_text <- sapply(strwrap(tooltip_text, 110, simplify=FALSE), paste, collapse="<br>" )
   
-  df <-  df %>%
-    arrange(desc(y))
-  
   highchart() %>%
-    
-# 
-# 
-# # for now filter just traffic and reasonable suspicion as separate dfs
-# 
-# df_traffic<-df%>%
-#   filter(grepl('Traffic Violation',stop_reason_short))%>%
-#   arrange(-hours_rate)
+    hc_add_series(df, 
+                  "item",
+                  rows=row_nums,
+                  hcaes(name=!!rlang::ensym(x), 
+                        y=!!rlang::ensym(y),
+                        label=!!rlang::ensym(x)),
+                  name="I DON'T KNOW WHAT THIS IS",
+                  showInLegend=TRUE)%>% # disables tooltip from popping up when mouse moves over bars
+    hc_colors(group_colors) %>%
+    hc_title(text=top_finding) %>%
+    hc_subtitle(text=subtitle) %>%
+    hc_caption(text=caption) %>%
+    hc_add_theme(cc_theme)%>%
+    hc_legend(title=list(text=paste0('<span style="color: #000000; font-weight: bold">', legend_text, '</span><br/><span style="color: #666; font-style: italic">Click to hide</span>')),
+              enable = TRUE,
+              labelFormat = paste0('{name} <span style="opacity: 0.4">{', y, ':.1f}</span>'))  %>%
+    hc_tooltip(headerFormat="",
+               pointFormat = tooltip_text) %>%
+    hc_exporting(enabled = TRUE, sourceWidth=900, sourceHeight=600,
+               chartOptions=list(plotOptions=list(series=list(dataLabels=list(enabled=TRUE,                                                           
+                                                                              format=paste0(list(pointFormat=paste0('{point.', y,':.1f}')))))),
+               filename = paste0(subtitle,"_Catalyst California, catalystcalifornia.org, 2023."))) %>%
+    hc_chart(
+      marginRight=100,
+      marginLeft=50)
+}
+  
 
-
-# mutate(hours_rate= round(hours_rate, 0))
-
-# col <-c(meteorite, lavender, orange, peridot, "#177FEB", "#733256", "#9B9A9A", "#211447",
-#         "#FF9E0D")
-# 
-# # mobile_screen_opts <- list(layout="vertical")
-# 
-# hc<-hchart(
-#   df_traffic,
-#   "item",
-#   rows=7,
-#   hcaes(
-#     name = stop_result_short,
-#     y = hours_rate,
-#     label = stop_result_short,
-#     color = col),
-#   name = "Total Hours",
-#   showInLegend = TRUE) %>%
-#   hc_title(text = "The majority of hours LBPD spends on traffic stops only result in citations, warnings, or no action.", 
-#            widthAdjust = -50,
-#            align="left" )%>%
-#   hc_subtitle(text = "Hours Spent on Traffic Stops by Result", 
-#               align="left" ) %>%
-#   hc_size(height=450) %>%
-#   hc_caption(text = paste0(sourcenote," Analysis for all officer-initiated stops for traffic violations.")) %>%
-#   hc_add_theme(cc_theme)%>%
-#   hc_exporting(enabled = TRUE, sourceWidth=900, sourceHeight=600,
-#                chartOptions=list(plotOptions=list(series=list(dataLabels=list(enabled=TRUE,                                                           format=paste0(list(pointFormat='{point.hours_rate:.1f}')))))),
-#                filename = paste0("Hours Spent on Traffic Stops by Result",
-#                                  "_Catalyst California, catalystcalifornia.org, 2023."))%>%
-#   
-#   hc_legend(title=list(text='<span style="color: #000000; font-weight: bold">Single dot represents 1 hour out of 100 hours</span><br/><span style="color: #666; font-style: italic">Click to hide</span>'),
-#             enable = TRUE,
-#             labelFormat = '{name} <span style="opacity: 0.4">{hours_rate:.1f}</span>')  %>%
-#   hc_tooltip(headerFormat="", 
-#              pointFormat = "Out of 100 hours, LBPD spent <b>{point.hours_rate:.1f}</b> hours on traffic stops that resulted in <b>{point.stop_result_short}</b>") %>%
-#   
-#   # add in margins so the graph isn't cut off
-#   hc_chart(
-#     marginRight = 20,
-#     marginLeft=20
-#   )
-# 
-# hc
-
-
-#### Testing Functions ####
-library(RPostgreSQL)
-source("W:\\RDA Team\\R\\credentials_source.R")
+#### testing item chart ####  
 pillars_conn <- connect_to_db("rjs_pillars")
-ourla_conn <- connect_to_db("pv_lareform")
-
-## Load in RJS Pillars data: Bubble Pop test ##
-report_stoprates_race_person <- dbGetQuery(pillars_conn, "SELECT * FROM data.report_gang_stoprates_race_person")  %>% filter(race!="total" & !is.na(stop_count))
-
-## Load in Our LA stacked data: Stacked Bar Chart test ##
-library(stringr)
-source("W:/Project/Political Voice/LA Reform/Phase 1/R/lareform_chart_prep/lareform_chart_prep/chart_styling.R")
-
-# Step 2a: pull df from postgres
-df <-dbGetQuery(ourla_conn, "SELECT * FROM report_effect_total") %>% 
-  filter(question %in% c("irc_10a", "irc_10b", "irc_10c")) %>% #filter for the question needed 
-  filter(value != "Unsure/Don't Know")  #filter out response options not of interest
-
-# Step 2b: pull data dictionary                     
-dict <-dbGetQuery(ourla_conn, "SELECT * FROM data_dictionary") %>% 
-  filter(variable %in% c("irc_10a", "irc_10b", "irc_10c")) #filter for the question needed 
-
-# Step 2c: merge databases for labels
-df_merge <- merge(df, dict, by.x = c("question"), by.y = c("variable"), all.x = TRUE)
-df_merge<-df_merge%>%rename(question_label=sub_question) #pull out question label for pop-up
-
-# Step 2d: The following levels set the order for the response options so they display correctly. Use the one that corresponds to the question you're visualizing for. 
-effect_levels<-c("Very positive effect","Slight positive effect","No effect","Slight negative effect","Very negative effect")
-effect_labels<-c("Very positive effect","Slight positive effect","No effect","Slight negative effect","Very negative effect")
-df_merge$value<-factor(df_merge$value, ordered=TRUE, levels=effect_levels, labels=effect_labels)
-
-# STEP 3: INSERT the text needed in the visual and follow the standards in this template. 
-
-# TITLE 
-title_text <- paste0("The majority of people believe an IRC will have a positive effect on their representation.")
-
-# SUBTITLE
-subtitle_text <- paste0("Percent of people by how they feel an IRC would impact their representation")
-
-# N CALCULATION REVISION
-n<-df%>%
-  group_by(question)%>%
-  mutate(n=min(total))%>%
-  slice(1)%>%
-  select(question, n)%>%
-  left_join(dict, by=c("question"="variable"))%>%
-  select("question", "n", "sub_question")
-
-# CAPTION 
-# take the question text from the data dictionary 
-question <- dict[1, "question"] #may need to use sub_question depending on what you're visualizing for
-
-# take the sample size from the data frame
-
-subquestion<-paste0(n[1,3]," (n=",n[1,2],")/",n[2,3]," (n=",n[2,2],")/",n[3,3]," (n=",n[3,2],")")
-
-caption_text <- paste0("<br>Survey Question: ", question," ",subquestion,"<br>",cc_footnote)
-
-# EXTRA TOOLTIP PREP 
-
-df_merge<-df_merge%>%
-  mutate(value_tooltip=ifelse(value!="No effect", paste0("a ","<b>",str_to_title(value),"</b>"),
-                              paste0("<b>",str_to_title(value),"</b>")))
-# TOOLTIP
-tooltip_text <- "<b>{point.rate:.1f}%</b> of people believe an IRC would have {point.value_tooltip} on <br><b>'{point.question_label}'</b>"
-
-
-## Load in RJS Pillars Data: Item Chart test ##
 timespent_ficard_race_person <- dbGetQuery(pillars_conn, "SELECT * FROM data.report_timespent_ficard_race_person") %>%
   filter(race != "overall")
-col <-c(meteorite, lavender, orange, peridot, ccblue, gainsboro, "#211447",
-        "#FF9E0D", "#A8683C")
 
+fx_itemchart(
+    df=timespent_ficard_race_person, 
+    x="race",
+    y="duration_rate", 
+    group_colors=c(meteorite, lavender, orange, peridot, ccblue, gainsboro, "#211447",
+    "#FF9E0D", "#A8683C"),
+    row_nums=7, # integer
+    top_finding="The top finding related to systemic factors",
+    subtitle= "The number of hours SDPD spent conducting field interviews by race",
+    tooltip_text="Out of 100 hours, LBPD spent <b>{point.duration_rate:.1f}</b> hours conducting field interviews on <b>{point.race} people</b>",
+    legend_text="Single dot represents 1 hour out of 100 hours",
+    caption = paste0(sourcenote," Analysis for all officer-initiated stops RESULTING IN FIELD INTERVIEWS ONLY."))    
 
-
-
-## Test Stacked Bar Chart ##
-
-
-## Test Item Chart ##
-hchart(
-  timespent_ficard_race_person,
-  "item",
-  rows=7,
-  hcaes(
-    name = race,
-    y = duration_rate,
-    label = race,
-    color = col),
-  name = "Total Hours",
-  showInLegend = TRUE) %>%
-  hc_title(text = "TOP SYSTEMIC FINDING STATEMENT", 
-           widthAdjust = -50,
-           align="left" )%>%
-  hc_subtitle(text = "Hours Spent on Field Interview Cards by Race", 
-              align="left" ) %>%
-  hc_size(height=450) %>%
-  hc_caption(text = paste0(sourcenote," Analysis for all officer-initiated stops RESULTING IN FIELD INTERVIEWS ONLY.")) %>%
-  hc_add_theme(cc_theme)%>%
-  hc_exporting(enabled = TRUE, sourceWidth=900, sourceHeight=600,
-               chartOptions=list(plotOptions=list(series=list(dataLabels=list(enabled=TRUE, format=paste0(list(pointFormat='{point.duration_rate:.1f}')))))),
-               filename = paste0("Hours Spent CONDUCTING FIELD INTERVIEWS BY RACE",
-                                 "_Catalyst California, catalystcalifornia.org, 2023."))%>%
+  dbDisconnect(pillars_conn)
   
-  hc_legend(title=list(text='<span style="color: #000000; font-weight: bold">Single dot represents 1 hour out of 100 hours</span><br/><span style="color: #666; font-style: italic">Click to hide</span>'),
-            enable = TRUE,
-            labelFormat = '{name} <span style="opacity: 0.4">{duration_rate:.1f}</span>')  %>%
-  hc_tooltip(headerFormat="", 
-             pointFormat = "Out of 100 hours, SDPD spent <b>{point.duration_rate:.1f}</b> hours ON STOPS CONDUCTING FIELD INTERVIWS OF <b>{point.race} PEOPLE</b>") %>%
+
+
+
+
   
-  # add in margins so the graph isn't cut off
-  hc_chart(
-    marginRight = 20,
-    marginLeft=20
-  )
